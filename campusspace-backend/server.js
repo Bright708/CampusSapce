b4c2580b345e1e47930d871f1b459221544ab5fd
Franklin_Saint <owurakufrimpong772@gmail.com>
2026-06-22 14:43:02 +0000

    `Update origin in CORS configuration to use FRONTEND_URL environment variable`

--- campusspace-backend/server.js
+++ campusspace-backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://campus-sapce.vercel.app/ ",
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     }),
 );
